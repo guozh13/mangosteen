fae6c47cba8b7595e0a6b3dd87a74cd5e22757d6
GuoZH <p870714@gmail.com>
2023-02-12 06:25:32 +0800

    tsx配置

---
+++ src/App2.tsx
@@ -0,0 +1,14 @@
+import { defineComponent, ref } from "vue";
+
+export const App = defineComponent({
+  setup() {
+    const count = ref(0)
+    const onClick = ()=>count.value++
+    return () => <><div>{count.value}</div>
+      <div>
+        <button onClick={onClick}>+</button>
+      </div>
+    </>
+
+  }
+})
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { App } from './App2'
 
 createApp(App).mount('#app')
